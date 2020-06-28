fixed cost added as an argument and agents was changed to utilize the fixed cost, method added to calculate the average prof per step for each agent.

agents.py
@@ -183,7 +183,7 @@ class Agents:
             ret.prod_cap = ret.q * ret.working_capital
             rand_value = np.random.exponential(scale = ret.consumer_demand_mean, size = 1)[0]
             ret.consumer_demand = rand_value                                   # Assigning consumer demand.
-            
+
             if ret.consumer_demand == 0:
                 continue
 
@@ -402,8 +402,7 @@ class Agents:
             if man.received_productions:
                 man.received_productions = list()
 
-        for sup in self.sup_list:  
-            # sup.fixed_cost = (1 - sup.q) * sup.working_capital
+        for sup in self.sup_list:
 
             if len(sup.customer_set) == 0:                                     # Making sure supplier  has received some orders.
                 sup.working_capital -= sup.fixed_cost
@@ -417,7 +416,7 @@ class Agents:
                     sup.step_production = sup.received_orders
 
                 if self.almost_equal_to_zero(sup.step_production, sup.abs_tol):
-                    # sup.working_capital -= sup.fixed_cost
+                    sup.working_capital -= sup.fixed_cost
                     continue
                 else:                        
                     sup.delivery_amount =[(agent_id, sup.step_production * 
@@ -429,7 +428,7 @@ class Agents:
                         agent.received_productions.append((
                             portion, sup.selling_price))                       # Kepping records of received products and their prices for customers.
 
-                    step_profit = (sup.input_margin * sup.step_production) - ((sup.interest_rate / 12) * sup.working_capital)  #Calculating profit using a fixed margin for suppliers
+                    step_profit = (sup.input_margin * sup.step_production) - ((sup.interest_rate / 12) * sup.working_capital) - sup.fixed_cost  #Calculating profit using a fixed margin for suppliers
                     sup.working_capital += step_profit                         # Updating suppliers' working capital.
 
     def deliver_to_retailers(self) -> None:
@@ -446,7 +445,6 @@ class Agents:
         for man in self.man_list:
             total_received_production = 0
             total_money_paid = 0
-            # man.fixed_cost = (1 - man.q) * man.working_capital
 
             for (amount, _) in man.received_productions:                       # Calculating total received productions.
                 total_received_production += amount                            # WHAT if the length is ZERO?
@@ -455,7 +453,7 @@ class Agents:
                 total_money_paid += amount * price
 
             if len(man.customer_set) == 0 or self.almost_equal_to_zero(total_received_production, man.abs_tol):       
-                # man.working_capital -= man.fixed_cost
+                man.working_capital -= man.fixed_cost
                 continue
             else:
                 if self._node_level_disruption:                                # Node level disruption is optional.
@@ -479,7 +477,7 @@ class Agents:
                             portion, man.selling_price))                      # Kepping records of received products and their prices for customers.
 
                     unit_production_cost = total_money_paid / total_received_production
-                    step_profit = (man.selling_price * man.step_production) - (unit_production_cost * man.step_production) - ((man.interest_rate / 12) * man.working_capital) #- man.fixed_cost
+                    step_profit = (man.selling_price * man.step_production) - (unit_production_cost * man.step_production) - ((man.interest_rate / 12) * man.working_capital) - man.fixed_cost
                     man.working_capital += step_profit
 
     def calculate_retailer_profit(self) -> None:
@@ -488,7 +486,6 @@ class Agents:
         receives an Agents object and manipulates retailers' working capital.
         """
         for ret in self.ret_list:
-            # ret.fixed_cost = (1 - ret.q) * ret.working_capital
             if ret.total_received_production:
                 ret.total_received_production = 0
 
@@ -502,21 +499,11 @@ class Agents:
             ret.total_received_production = total_received_production
 
             if self.almost_equal_to_zero(ret.total_received_production, ret.abs_tol):         
-                # ret.working_capital -= ret.fixed_cost
+                ret.working_capital -= ret.fixed_cost
                 continue
             else:
                 unit_production_cost = total_money_paid / ret.total_received_production
-                step_profit = (ret.selling_price * ret.total_received_production) - (unit_production_cost * ret.total_received_production) - ((ret.interest_rate / 12) * ret.working_capital) #- ret.fixed_cost
-                # print(f' selling_price: {ret.selling_price}')
-                # print(f' step_production: {ret.total_received_production}')
-                # print(f'majmoe: {ret.selling_price * ret.total_received_production}')
-                # print(f'unit_production_cost: {unit_production_cost}')
-                # print(f' step_production: {ret.total_received_production}')
-                # print(f'majmoe: {unit_production_cost * ret.total_received_production}')
-                # print(f' cost of capital: {(ret.interest_rate / 12)}')
-                # print(f' working capital: {ret.working_capital}')
-                # print(f'majmoe: {(ret.interest_rate / 12) * ret.working_capital}')
-                # print(f' step_profit: {step_profit}')
+                step_profit = (ret.selling_price * ret.total_received_production) - (unit_production_cost * ret.total_received_production) - ((ret.interest_rate / 12) * ret.working_capital) - ret.fixed_cost
                 ret.working_capital += step_profit
 
     def upstream_flow(self) -> None: